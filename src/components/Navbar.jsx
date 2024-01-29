9cd276d40bb53e90138bcc70cb7ee9fb4172c90c
Abel Bright <oluwasegunbarnabas@gmail.com>
2024-01-29 12:01:00 +0100

    edited

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -32,7 +32,7 @@ const Navbar = () => {
     left: 0,
     width: '100%',
     // padding: '10px',
-    backgroundColor: isNavbarTransparent ? 'transparent' : '#f1f5f9df',
+    backgroundColor: isNavbarTransparent ? 'transparent' : '#f0fdfae1',
     transition: 'background-color 0.3s ease-in-out',
   };
 
